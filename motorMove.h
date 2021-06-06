e8d83ac7e14c7bf42d2c5d8611160228bc7189d3
HYORIMLEE <37135345+HYORIMLEE@users.noreply.github.com>
2021-06-06 17:08:20 +0900

    update motorTask

--- motorMove.h
+++ motorMove.h
@@ -5,12 +5,18 @@
 
 #define MOTOR_TASK_STK_SIZE 128
 #define MOTOR_TASK_PRIO 2
-#define MOTOR_PORT GPIOE
-#define MOTOR_PNUM 3
 
 static OS_TCB motor_tcb;
 static CPU_STK motor_stack[MOTOR_TASK_STK_SIZE];
 
-int moveMotor();
+typedef struct {
+    GPIO_TypeDef* gpioOne;
+    GPIO_TypeDef* gpioTwo;
+    u16 pinOne;
+    u16 pinTwo;
+}motor;
+struct motor motorA[2],motorB[2];
+void initMotor();
+void motorTask(void* parg);
 
 #endif
---
+++ term.h
@@ -0,0 +1,18 @@
+#ifndef TERM
+#define TERM
+//CAT_STATE
+int CAR_STATE = 0;
+/*
+0 : STOP;
+1 : GO FOWARD;
+2 : GO LEFT;
+3 : GO RIGHT;
+*/
+#define STOP 0
+#define GO_FORWARD 1
+#define GO_LEFT 2
+#define GO_RIGHT 3
+
+
+
+#endif
