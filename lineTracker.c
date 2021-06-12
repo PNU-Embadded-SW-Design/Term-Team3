2ca5dc946bc05f37c0e13ba29fcd1f14d0935c80
zetwhite <zetwhite@naver.com>
2021-06-13 00:42:13 +0900

    modify lineTracker port setting

--- lineTracker.c
+++ lineTracker.c
@@ -1,26 +1,56 @@
 #include "lineTracker.h"
+#include <stdio.h> 
 
-int isBlack(){
-  if(GPIOE->IDR & GPIO_IDR_IDR13){
-    return 1; 
-  }
-  else
-     return 0; 
+/*int isBlack(){
+    if(GPIOB->IDR & GPIO_IDR_IDR13){
+        return 1; 
+    }
+    else
+        return 0; 
+}*/
+
+enum CAR_DIR getDirection(){
+    int rightBlack = !(GPIOB->IDR & R_SENSE); 
+    int leftBlack = !(GPIOB->IDR & L_SENSE); 
+    if(rightBlack && leftBlack){
+        #ifdef _DEBUG
+        printf("Straight!\n"); 
+        #endif 
+        return FORWARD; 
+    }
+    else if( rightBlack && !leftBlack){
+        #ifdef _DEBUG
+        printf("Left\n"); 
+        #endif
+        return LEFT; 
+    }
+    else if ( !rightBlack && leftBlack){
+      #ifdef _DEBUG
+      printf("Right\n"); 
+      #endif
+      return RIGHT; 
+    }
+    #ifdef _DEBUG 
+    printf("OutOfWay\n"); 
+    #endif
+    return RIGHT; 
 }
 
 
 void initLineTracker(){
     RCC_MCOConfig(RCC_MCO_SYSCLK);      
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE, ENABLE); //portE
-    
-    GPIOE->IDR |= GPIO_IDR_IDR13; //forLineTracker
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE); //portE
+    GPIOB->IDR |= R_SENSE; //forLineTracker1
+    GPIOB->IDR |= L_SENSE; //forLineTracker2
 }
 
 void lineTrackTask(void* parg){
     initLineTracker(); 
     OS_ERR err; 
-  
+
     while(1){
+        gCarDir = getDirection(); 
+        /*
         if(isBlack()){
             //printf("on"); 
             BSP_LED_On(2); 
@@ -28,7 +58,7 @@ void lineTrackTask(void* parg){
         else{
             //printf("off");
             BSP_LED_Off(2); 
-        }
+        }*/
         OSTimeDlyHMSM(0, 0, 0, 100, OS_OPT_TIME_HMSM_STRICT, &err);
     }
 }
--- lineTracker.h
+++ lineTracker.h
@@ -2,18 +2,19 @@
 #define LINE
 
 #include "includes.h"
-
-#define LINE_TASK_STK_SIZE 1024
-#define LINE_TASK_PRIO 2
-#define LINE_PORT GPIOE
-#define LINE_PNUM 13
+#include "term.h" 
+#define LINE_TASK_STK_SIZE      1024
+#define LINE_TASK_PRIO          2
+#define LINE_PORT               GPIOB
+#define R_SENSE                 GPIO_IDR_IDR12
+#define L_SENSE                 GPIO_IDR_IDR13
 
 
 static OS_TCB line_tcb; 
 static CPU_STK line_stack[LINE_TASK_STK_SIZE]; 
 
 
-int isBlack(); 
+//int isBlack(); 
 
 void initLineTracker(); 
 void lineTrackTask(void* parg); 
